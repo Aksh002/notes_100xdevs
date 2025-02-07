6b5c4f251057e1ad55a5db4ebd30f831a69b32ad
AkshitGangwar <akshitgangwar02@gmail.com>
2025-02-07 23:51:52 +0530

    Scheduled push of 7/1/25

--- Week-14/14.1-Next.js-Intro/intro-next-app/components/Signin.tsx
+++ Week-14/14.1-Next.js-Intro/intro-next-app/components/Signin.tsx
@@ -1,5 +1,6 @@
-export default function Signin() {
-  return <div className="h-screen flex justify-center flex-col">
+export const SigninComponent=()=>{
+    return <div>
+        <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
       <a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 ">
               <div>
@@ -17,17 +18,19 @@ export default function Signin() {
           </a>
       </div>
   </div>
+    </div>
 }
+
 
 interface LabelledInputType {
-  label: string;
-  placeholder: string;
-  type?: string;
-}
-
-function LabelledInput({ label, placeholder, type }: LabelledInputType) {
-  return <div>
-      <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
-      <input type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
-  </div>
-}
+    label: string;
+    placeholder: string;
+    type?: string;
+  }
+  
+  function LabelledInput({ label, placeholder, type }: LabelledInputType) {
+    return <div>
+        <label className="block mb-2 text-sm text-black font-semibold pt-4">{label}</label>
+        <input type={type || "text"} id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
+    </div>
+  }
