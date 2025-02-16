f7f2a37c31c1149dfee1f419cde9d7e22a2f4015
Aksh002 <akshitgangwar02@gmail.com>
2025-02-16 18:35:12 +0530

    Scheduled push of 16/2/25

--- Week-14/14.1-Next.js-Intro/intro-next-app/components/SigninComp.tsx
+++ Week-14/14.1-Next.js-Intro/intro-next-app/components/SigninComp.tsx
@@ -1,3 +1,6 @@
+
+import SigninButton from "./buttons/SigninButton";
+
 export const SigninComponent=()=>{
     return <div>
         <div className="h-screen flex justify-center flex-col">
@@ -12,7 +15,7 @@ export const SigninComponent=()=>{
                   <div className="pt-2">
                       <LabelledInput label="Username" placeholder="harkirat@gmail.com" />
                       <LabelledInput label="Password" type={"password"} placeholder="123456" />
-                      <button type="button" className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Sign in</button>
+                      <SigninButton></SigninButton>
                   </div>
               </div>
           </a>
