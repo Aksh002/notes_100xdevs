0d6d7c4a562cb1bdac028c795842f1168e3af67c
Aksh002 <akshitgangwar02@gmail.com>
2025-06-08 02:05:30 +0530

    scheduled push of 8/6/25

--- Week-16/16.2-Turborepos/learningTurborepo/packages/ui/src/admin.tsx
+++ Week-16/16.2-Turborepos/learningTurborepo/packages/ui/src/admin.tsx
@@ -2,7 +2,7 @@ export const Admin=()=> {
   return (
     <div>
       <h1>Admin Page</h1>
-      <p>This is the admin page of the application.</p>
+      <p>This is the admin page of the application.HELOOOOOOOO</p>
     </div>
   );
 }
